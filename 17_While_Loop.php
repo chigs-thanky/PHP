c1ea0ff5ca00a77cb9092e0ffce9afc89aeaf361
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2024-09-19 11:04:51 +0530

    While loop

---
+++ 17_While_Loop.php
@@ -0,0 +1,15 @@
+<?php
+    // Increment
+    // $a = 1;
+    // while ($a <= 10) {
+    //     echo "$a) Hello World<br>";
+    //     $a++;
+    // }
+
+    // Decrement
+    $a = 50;
+    while ($a >= 1){
+        echo "$a) Hello World <br>";
+        $a--;
+    }
+?>
